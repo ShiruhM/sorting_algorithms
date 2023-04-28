5be60f6c365879c9734b32a42f0280e080e9f902
Maureen Wanjiru <mourynewanjiru@gmail.com>
2023-04-28 15:08:46 +0000

    task 103

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -64,11 +64,12 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
 	}
 }
 
-
 /**
- * merge_sort - Sort an array of integers in ascending order using the merge sort algorithm.
+ * merge_sort - Sort an array of integers in ascending
+ *              order using the merge sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
+ *
  * Description: Implements the top-down merge sort algorithm.
  */
 
